79aeeb3fb604e68b2d985cbfe3fe3952a97f63dd
BencenoMrX <98375925+BencenoMrX@users.noreply.github.com>
2026-05-19 10:35:54 -0300

    Update pka_predictor.py
    Change Morgan fingerprint code to update according to rdkit

--- pka_predictor.py
+++ pka_predictor.py
@@ -1,27 +1,32 @@
-# Save this file as pka_predictor.py in your GitHub repo
+# pka_predictor class
 import os
 import joblib
 import numpy as np
 from rdkit import Chem
-from rdkit.Chem import AllChem, Descriptors
+from rdkit.Chem import AllChem, Descriptors, rdFingerprintGenerator
 
 class PkaPredictor:
     def __init__(self, model_path='pka_lightgbm_model.pkl'):
+        # Loads model
         if not os.path.exists(model_path):
             raise FileNotFoundError(f"Model file not found at {model_path}")
+        
         self.model = joblib.load(model_path)
+        
+        # Initialize the Morgan generator once, using new Morgan rdkit code
+        self.morgan_gen = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=1024)
         
     def _extract_features(self, smiles):
         try:
             mol = Chem.MolFromSmiles(smiles)
             if mol is None: return None
             
-            # 1. 1024-bit Morgan Fingerprint
-            fp = AllChem.GetMorganFingerprintAsBitVect(mol, radius=2, nBits=1024)
+            # 1024-bit Morgan fingerprint
+            fp = self.morgan_gen.GetFingerprint(mol)
             fp_arr = np.zeros((1,), dtype=np.float32)
             Chem.DataStructs.ConvertToNumpyArray(fp, fp_arr)
             
-            # 2. Gasteiger Charges
+            # 2. Gasteiger charges (partial atomic charges)
             mol_h = Chem.AddHs(mol)
             AllChem.ComputeGasteigerCharges(mol_h)
             charges = [float(a.GetProp('_GasteigerCharge')) for a in mol_h.GetAtoms() 
@@ -29,7 +34,7 @@ class PkaPredictor:
             max_charge = max(charges) if charges else 0.0
             min_charge = min(charges) if charges else 0.0
             
-            # 3. All RDKit Descriptors
+            # RDKit Descriptors
             rdkit_desc_values = []
             for desc_name, desc_func in Descriptors._descList:
                 try:
@@ -46,7 +51,7 @@ class PkaPredictor:
     def predict(self, smiles):
         features = self._extract_features(smiles)
         if features is None:
-            return "Invalid Structure"
+            return "Invalid structure"
         return round(float(self.model.predict([features])[0]), 2)
         
     def predict_batch(self, smiles_list):
