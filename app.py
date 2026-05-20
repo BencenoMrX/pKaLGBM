a9f750fc7f01bf925ad003a0a48da4d2a9e97da4
BencenoMrX <98375925+BencenoMrX@users.noreply.github.com>
2026-05-20 01:15:37 -0300

    Update app.py
    Added scrolling false to HTML

--- app.py
+++ app.py
@@ -155,8 +155,8 @@ with col2:
                 pdb2 = build_3d_molecule(smiles2, df_joint, use_experimental)
                 if pdb1 and pdb2:
                     view = render_3d_surface(pdb1, pdb2)
-                    components.html(view._make_html(), height=500, width=800)
+                    components.html(view._make_html(), height=500, width=800, scrolling=False)
             else:
                 if pdb1:
                     view = render_3d_surface(pdb1)
-                    components.html(view._make_html(), height=500, width=800)
+                    components.html(view._make_html(), height=500, width=800, scrolling=False)
